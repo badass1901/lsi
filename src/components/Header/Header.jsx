994505e609c354783db6b693bb6a9136084eed94
Shashwat Sagar <shashwatsagar19@gmail.com>
2023-04-09 17:19:16 +0530

    working on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,43 @@
 import React from "react";
 import "./Header.scss";
+import Drawer from "../Drawer";
+import { Link } from "react-router-dom";
 
 import { InfoIcon, ShoppingCartIcon, PhoneIcon } from "../../utils/icons";
+import { Divider } from "@mui/material";
 
 const Header = () => {
   return (
-    <header className="header w-full p-2">
+    <header className="header flex justify-between w-full px-20 py-4">
       <div className="header__logo">
         <img
+          className=" w-56"
           src="https://localshopindia.com/images/logo/logo-header.png"
           alt="LSI Logo"
         />
       </div>
-      <ul className="header__menu">
-        <li className="cart">
-          <ShoppingCartIcon />
-          <span>Cart</span>
-        </li>
-        <li className="About">
-          <InfoIcon />
-          <span>About Us</span>
-        </li>
-        <li className="Contact">
-          <PhoneIcon />
-          <span>Contat</span>
-        </li>
-        <li className="Sign">
-          <PhoneIcon />
-          <span>Contat</span>
-        </li>
+      {/* <Drawer /> */}
+      <ul className="header__menu w-1/3 flex flex-col md:flex-row lg:flex-row justify-normal md:justify-evenly lg:justify-evenly">
+        <Link to="/cart">
+          <li className="cart">
+            <ShoppingCartIcon />
+            <span>Cart</span>
+          </li>
+        </Link>
+        <Link to="/contact">
+          <li className="contact">
+            <PhoneIcon />
+            <span>Contact</span>
+          </li>
+        </Link>
+        <Link to="/about">
+          <li className="about">
+            <InfoIcon />
+            <span>About</span>
+          </li>
+        </Link>
       </ul>
+      <hr />
     </header>
   );
 };
